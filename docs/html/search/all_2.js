41820847e293047f40e732b6943a79bf0495731f
SnailDragon <tylernielsenn@gmail.com>
2024-09-14 21:54:23 -0700

    created potential doxyfile, wip

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['sdstorage_8',['SDStorage',['../classSDStorage.html',1,'SDStorage'],['../classSDStorage.html#a992cb710f41462a7a35c6dfdc838f01f',1,'SDStorage::SDStorage()']]],
+  ['sensor_9',['Sensor',['../classSensor.html',1,'Sensor'],['../classSensor.html#ac961611fe726ecce2236efa3b9ee450c',1,'Sensor::Sensor(String sensor_name, String csv_header, int fields)'],['../classSensor.html#a861ed177b65f0d04543e7b6fae6b8c85',1,'Sensor::Sensor(String sensor_name, String csv_header, int fields, unsigned long minimum_period)']]],
+  ['setlastexecution_10',['setLastExecution',['../classSensor.html#aea739069d8009fdb9b4026b4b3afc3ab',1,'Sensor']]],
+  ['setperiod_11',['setPeriod',['../classSensor.html#aecd2cfc8a5d16e306f6369adcc3e208a',1,'Sensor']]],
+  ['storage_12',['Storage',['../classStorage.html',1,'']]],
+  ['store_13',['store',['../classSDStorage.html#a9450318396c536da92d44d9ebad33ec1',1,'SDStorage::store()'],['../classStorage.html#a08674ceb4404690065b1cf39963eb5a3',1,'Storage::store()']]]
+];
---
+++ docs/html/search/functions_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['sdstorage_28',['SDStorage',['../classSDStorage.html#a992cb710f41462a7a35c6dfdc838f01f',1,'SDStorage']]],
+  ['sensor_29',['Sensor',['../classSensor.html#ac961611fe726ecce2236efa3b9ee450c',1,'Sensor::Sensor(String sensor_name, String csv_header, int fields)'],['../classSensor.html#a861ed177b65f0d04543e7b6fae6b8c85',1,'Sensor::Sensor(String sensor_name, String csv_header, int fields, unsigned long minimum_period)']]],
+  ['setlastexecution_30',['setLastExecution',['../classSensor.html#aea739069d8009fdb9b4026b4b3afc3ab',1,'Sensor']]],
+  ['setperiod_31',['setPeriod',['../classSensor.html#aecd2cfc8a5d16e306f6369adcc3e208a',1,'Sensor']]],
+  ['store_32',['store',['../classSDStorage.html#a9450318396c536da92d44d9ebad33ec1',1,'SDStorage::store()'],['../classStorage.html#a08674ceb4404690065b1cf39963eb5a3',1,'Storage::store()']]]
+];
