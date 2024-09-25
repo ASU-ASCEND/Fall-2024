9adec91c0e4b8bdc3772504b89d1ab31e0ebba6e
P M <pm@ps-macbook-pro.3521.dhcp.asu.edu>
2024-09-25 16:56:35 -0700

    Migrated LSM Sensor

---
+++ ascendfsw/include/LSM9DS1Sensor.h
@@ -0,0 +1,17 @@
+#ifndef LSM9DS1SENSOR_H
+#define LSM9DS1SENSOR_H
+
+#include "Sensor.h"
+#include <string.h>
+#include <Arduino_LSM9DS1.h>
+
+class LSM9DS1Sensor : public Sensor {
+    private:
+    public:
+        LSM9DS1Sensor();
+        LSM9DS1Sensor(unsigned long minimum_period);
+        bool verify() override;
+        String readData() override;
+};
+
+#endif
