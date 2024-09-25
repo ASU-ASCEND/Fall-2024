44d842ff34a6a06f018d10df37a8047daae95faf
Karen Banda <kerynbe@karens-macbook.3520.dhcp.asu.edu>
2024-09-25 16:28:42 -0700

    migrated sensor files

---
+++ ascendfsw/include/SHT31Sensor.h
@@ -0,0 +1,23 @@
+#ifndef SHT31SENSOR_H
+#define SHT31SENSOR_H
+
+#include "Sensor.h"
+#include <string.h>
+#include <Arduino.h>
+#include "Adafruit_SHT31.h"
+#include <Wire.h>
+
+class SHT31Sensor : public Sensor {
+    private:
+        Adafruit_SHT31 sht31;
+        String nameCompiled = "SHT31";
+        String csvHeaderCompiled = "SHT31 Hum %, SHT31 Temp C, ";
+    public:
+        SHT31Sensor();
+        SHT31Sensor(unsigned long minimum_period);
+
+        bool verify() override;
+        arduino::String readData() override;
+};
+
+#endif
---
+++ ascendfsw/src/SHT31Sensor.cpp
@@ -0,0 +1,23 @@
+#include "SHT31Sensor.h"
+
+const arduino::String& SHT31Sensor::getSensorName() const {
+    return nameCompiled;
+}
+
+const arduino::String& SHT31Sensor::getSensorCSVHeader() const {
+    return csvHeaderCompiled;
+}
+
+bool SHT31Sensor::verify() {
+    sht31 = Adafruit_SHT31();
+    return sht31.begin(0x44);
+    // return false;
+}
+
+// returns in format Hum, Temp, 
+arduino::String SHT31Sensor::readData() {
+    // return "-. -. ";
+    float t = sht31.readTemperature();
+    float h = sht31.readHumidity();
+    return String(h) + ", " + String(t) + ", ";
+}
