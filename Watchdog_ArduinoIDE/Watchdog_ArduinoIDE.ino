f3f7dcf1c5d59a5b835d27848aba1a066c7c60ce
SnailDragon <tylernielsenn@gmail.com>
2024-10-13 18:01:03 -0700

    resetting with npn works

--- Watchdog_ArduinoIDE/Watchdog_ArduinoIDE.ino
+++ Watchdog_ArduinoIDE/Watchdog_ArduinoIDE.ino
@@ -1,29 +1,29 @@
 
-#define DATA_HEARTBEAT_PIN 5
-#define STORAGE_HEARTBEAT_PIN 6
-#define RESET_PIN 2
+#define DATA_HEARTBEAT_PIN 3
+#define STORAGE_HEARTBEAT_PIN 4
+#define RESET_PIN 0
 
 volatile bool data_beat = 0;
 volatile bool storage_beat = 0; 
 
-void PCINT0_ISR(void){
+// void PCINT0_ISR(void){
 
+// }
+
+void setup(){
+  // setup 
+  // pinMode(DATA_HEARTBEAT_PIN, INPUT); 
+  // pinMode(STORAGE_HEARTBEAT_PIN, INPUT); 
+  pinMode(RESET_PIN, OUTPUT); 
+  // digitalWrite(RESET_PIN, LOW); 
 }
 
-int main(){
-  // setup 
-  pinMode(DATA_HEARTBEAT_PIN, INPUT); 
-  pinMode(STORAGE_HEARTBEAT_PIN, INPUT); 
-  pinMode(RESET_PIN, OUTPUT); 
+void loop(){
+
+  delay(10000); 
+  digitalWrite(RESET_PIN, HIGH); 
+  delay(1000); 
   digitalWrite(RESET_PIN, LOW); 
-
-  while(1){
-    delay(1000); 
-    digitalWrite(RESET_PIN, HIGH); 
-    delay(1000); 
-    digitalWrite(RESET_PIN, LOW); 
-
-  }
   // // any logical change interrupt 
   // MCUCR &= ~(0b11);
   // MCUCR |= (0b01);
@@ -46,5 +46,4 @@ int main(){
   //   storage_beat = false; 
   // }
 
-  return 0; 
 }
