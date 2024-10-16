6ce1bd32592ab2849cb67589ecae9f86ffbcb3e7
hsing117 <145151413+hsing117@users.noreply.github.com>
2024-10-16 14:58:44 -0700

    Add files via upload

---
+++ ascendfsw/src/PCF8523Sensor.cpp
@@ -0,0 +1,22 @@
+
+#include "PCF8523Sensor.h"
+
+PCF8523Sensor::PCF8523Sensor() {}
+
+bool PCF8523Sensor::begin() {
+    return rtc.begin();
+}
+
+DateTime PCF8523Sensor::now() {
+    return rtc.now();
+}
+
+void PCF8523Sensor::adjust(const DateTime& dt) {
+    rtc.adjust(dt);
+}
+
+void PCF8523Sensor::calibrate() {
+    if (!rtc.initialized() || rtc.lostPower()) {
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
+}
