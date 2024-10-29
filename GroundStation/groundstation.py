ceca0da3a58a6f0b2c11d1456e740337dac36637
SnailDragon <tylernielsenn@gmail.com>
2024-10-29 15:00:14 -0700

    potential code, untested

--- GroundStation/groundstation.py
+++ GroundStation/groundstation.py
@@ -1,6 +1,7 @@
 import serial
 from datetime import datetime
 import serial.tools.list_ports as sp
+import tkinter as tk
 
 print("Starting Ground Station...")
 print(f"Time: {datetime.now().strftime("%H:%M:%S")}\n")
@@ -34,8 +35,35 @@ file = open(fileName, "a")
 file.close()
 print(fileName + " created to hold data. If file exists, data will be appended\n")
 
-with open(fileName, "a", newline = '\n') as f:
-  input = ser.readline()
+root = tk.Tk()
+root.title("MSGC RFD900x")
 
-  print(input)
-  f.write(input + "\n")
+data_line = ""
+last_line = ""
+def update():
+  with open(fileName, "a", newline = '\n') as f:
+    last_line = data_line
+    data_line = ser.readline()
+
+    print(data_line)
+    f.write(data_line + "\n")
+
+  Title = tk.Label(root, font = ("Helvetica", "20"))
+  Title.grid(row=0,column=0,padx=(0, 0), pady=(0,0))
+  Title.config(text=('RFD-900x Data'))
+
+  now = datetime.datetime
+
+  Data1 = tk.Label(root, font = ("Helvetica", "10"))
+  Data1.grid(row=1,column=0,padx=(0, 5), pady=(0,0))
+  Data1.config(text=f"{datetime.now().strftime("%H:%M:%S")}--> {last_line}")
+
+  Data2 = tk.Label(root, font = ("Helvetica", "10"))
+  Data2.grid(row=1,column=1,padx=(5, 0), pady=(0,0))
+  Data2.config(text=f"{datetime.now().strftime("%H:%M:%S")}--> {data_line}")
+
+  root.after(500, update) 
+
+root.after(0, update)
+
+root.mainloop()
