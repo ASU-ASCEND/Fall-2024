6d7f8c377ff09d2ec4f568cfff29d23c1cac2826
Pablo Minjarez <pminjare@asu.edu>
2024-10-30 13:19:01 -0700

    Update to Adafruit LSM9DS1 library

--- ascendfsw/include/LSM9DS1Sensor.h
+++ ascendfsw/include/LSM9DS1Sensor.h
@@ -1,7 +1,8 @@
 #ifndef LSM9DS1SENSOR_H
 #define LSM9DS1SENSOR_H
 
-#include <SparkFunLSM9DS1.h>  // Updated to use the SparkFun library
+#include <Adafruit_LSM9DS1.h>      // Updated to use the Adafruit library
+#include <Adafruit_Sensor.h>
 #include <string.h>
 
 #include "Sensor.h"
@@ -12,14 +13,19 @@
  */
 class LSM9DS1Sensor : public Sensor {
  private:
-  LSM9DS1 IMU;  // Declaring an instance of the LSM9DS1 class
+  Adafruit_LSM9DS1 lsm;  // Using Adafruit_LSM9DS1 instead of SparkFunLSM9DS1
+
+ void setupSensor();    //Function to set up sensor ranges and data rates
+  float accel_offsets[3];  //Offsets for accelerometer calibration
+  float gyro_offsets[3];   //Offsets for gyroscope calibration
+  float mag_offsets[3];    //Offsets for magnetometer calibration
 
  public:
   LSM9DS1Sensor();
   LSM9DS1Sensor(unsigned long minimum_period);
   bool verify() override;
   String readData() override;
-  void calibrate();  // New calibration function
+  void calibrate();  // calibration function
 };
 
 #endif
