8c18b7196bcc83a7862cb36e239078726f00c00e
SnailDragon <tylernielsenn@gmail.com>
2024-10-31 20:12:43 -0700

    potential groundstation tkinter code

--- GroundStation/groundstation.py
+++ GroundStation/groundstation.py
@@ -12,12 +12,22 @@ print(f"Time: {current_time}\n")
 # potential full header, based on sensors in flight software main, used to build header
 # avoids trouble from missing transmit header 
 full_no_fail_header = [
-   "header1","header2",
+  "Header","Millis",
+  "BMETemp(C),BMEPress(hPa),BMEHum(%),BMEGas(KOhms),BMEAlt(m),",    
+  "GeigerSensor(CPS),", 
+  "INACurr(mA),INAVolt(mV),INAPow(mW),",                   
+  "LSM9DS1 AccX, LSM9DS1 AccY, LSM9DS1 AccZ, LSM9DS1 GyroX, LSM9DS1 GyroY, LSM9DS1 GyroZ, LSM9DS1 MagX, LSM9DS1 MagY, LSM9DS1 MagZ,", 
+  "SHTHum(%), SHTTemp(C),",  
+  "PicoTemp(C),",
+  "WindADC,TempADC,", 
+  "SGPTVOC(ppb),SGPeCO2(ppm),SGPTVOCBase(ppb),SGPeCO2Base(ppb),SGPH2,SGPEtha",
+  "BME280RelHum %,BME280Pres Pa,BME280Alt m,BME280TempC,DewPointC,",  
+  "ENSAQI,ENSTVOC ppb,ENSECO2 ppm,",
   " "  # final space for matching fsw
 ]
 
-# root = tk.Tk()
-# root.title("MSGC RFD900x")
+root = tk.Tk()
+root.title("ASU ASCEND Ground Station")
 
 # User enter serial port
 ports = list(sp.comports())
@@ -42,39 +52,58 @@ file = open(fileName, "a")
 file.close()
 print(fileName + " created to hold data. If file exists, data will be appended\n")
  
-while True:
+HIST_SIZE = 6
+data_line = ["empty," for i in range(HIST_SIZE)]
+time_line = ["none" for i in range(HIST_SIZE)]
+header = "Receive time,header,"
+
+def update():
   with open(fileName, "a", newline = '\n') as f:
-    data_line = ser.readline()
+    # last_line = data_line
+    data_line.insert(0, str(ser.readline()))
+    data_line.pop() 
 
-    print(data_line)
-    f.write(data_line + "\n")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    time_line.insert(0, current_time)
+    time_line.pop()
 
-# data_line = ""
-# last_line = ""
-# def update():
-  # with open(fileName, "a", newline = '\n') as f:
-  #   last_line = data_line
-  #   data_line = "asdfasdfads" #ser.readline()
+    if(header == "header"):
+      header = "Receive time, "
+      header_field = int(data_line[0].split(",")[0], 16) # read header field, convert from hex 
+      header_bin = bin(header_field)[2:]
+      header_bin = header_bin[header_bin.find("1"):] # trim to first 1
+      for i in range(len(header_bin)): # add each from full header if 1
+        if header_bin[i] == "1": header += full_no_fail_header[i]
+      print(",".join(header))
+      f.write(",".join(header) + "\n")
 
-  #   print(data_line)
-  #   f.write(data_line + "\n")
+    print(f"{time_line[0]}, {data_line[0]}")
+    f.write(f"{time_line[0]}, {data_line[0]}\n")
 
-#   Title = tk.Label(root, font = ("Helvetica", "20"))
-#   Title.grid(row=0,column=0,padx=(0, 0), pady=(0,0))
-#   Title.config(text=('RFD-900x Data'))
+  Title = tk.Label(root, font = ("Helvetica", "20", "bold"))
+  Title.grid(row=0,column=0,padx=(0, 0), pady=(0,0))
+  Title.config(text=('RFD-900x Data'))
 
-#   now = datetime.datetime
+  # Data1 = tk.Label(root, font = ("Helvetica", "10"))
+  # Data1.grid(row=1,column=0,padx=(0, 5), pady=(0,0))
+  # Data1.config(text=f"{header}")
+  header_arr = header.split(",")
+  for i in range(len(header_arr)):
+    header_label = tk.Label(root, font = ("Helvetica", "10", "bold"))
+    header_label.grid(row=1, column=i, padx=5, pady=0)
+    header_label.config(text=header_arr[i])
 
-#   Data1 = tk.Label(root, font = ("Helvetica", "10"))
-#   Data1.grid(row=1,column=0,padx=(0, 5), pady=(0,0))
-#   Data1.config(text=f"{last_line}")
+  for i in range(len(data_line)):
+    combined_arr = f"{time_line[i]},{data_line[i]}".split(",")
 
-#   Data2 = tk.Label(root, font = ("Helvetica", "10"))
-#   Data2.grid(row=1,column=1,padx=(5, 0), pady=(0,0))
-#   Data2.config(text=f"{data_line}")
+    for j in range(len(combined_arr)):
+      Data = tk.Label(root, font = ("Helvetica", "10"))
+      Data.grid(row=i+2, column=j, padx=5, pady=0)
+      Data.config(text=combined_arr[j])
 
-#   root.after(500, update) 
+  root.after(500, update) 
 
-# root.after(0, update)
+root.after(0, update)
 
-# root.mainloop()
+root.mainloop()
