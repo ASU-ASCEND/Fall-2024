7929a504587912e6d9a2574e06ca5b7b7ca116a6
SnailDragon <tylernielsenn@gmail.com>
2024-11-08 16:04:05 -0700

    added logging to executable

--- GroundStation/groundstation.py
+++ GroundStation/groundstation.py
@@ -6,6 +6,37 @@ import tkinter as tk
 import threading
 import sys
 import os
+import logging
+
+#----------------- error stuff
+class StreamToLogger(object):
+  """
+  Fake file-like stream object that redirects writes to a logger instance.
+  """
+  def __init__(self, logger, log_level=logging.INFO):
+    self.logger = logger
+    self.log_level = log_level
+    self.linebuf = ''
+
+  def write(self, buf):
+    for line in buf.rstrip().splitlines():
+          self.logger.log(self.log_level, line.rstrip())
+
+logging.basicConfig(
+level=logging.DEBUG,
+format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+filename="error_output.log",
+filemode='a'
+)
+
+# stdout_logger = logging.getLogger('STDOUT')
+# sl = StreamToLogger(stdout_logger, logging.INFO)
+# sys.stdout = sl
+
+stderr_logger = logging.getLogger('STDERR')
+sl = StreamToLogger(stderr_logger, logging.ERROR)
+sys.stderr = sl
+#------------------
 
 print("Starting Ground Station...")
 now = datetime.now()
