421c35d0d7471263f16f655b25571ab76591981c
SnailDragon <tylernielsenn@gmail.com>
2024-11-09 16:03:52 -0700

    added doxygen comments

--- ascendfsw/include/PayloadConfig.h
+++ ascendfsw/include/PayloadConfig.h
@@ -1,47 +1,59 @@
 #ifndef PAYLOAD_CONFIG_H
 #define PAYLOAD_CONFIG_H
 
-/**
- * @brief Central location for pinout and config defines
- *
- */
+/** Central location for pinout and config defines */
 
-// error display
+/** Error display bit 2 */
 #define ERROR_PIN_2 2
+/** Error display bit 1 */
 #define ERROR_PIN_1 3
+/** Error display bit 0 */
 #define ERROR_PIN_0 6
 
 // sensors
-// bme 680
+/** BME680 CS Pin */
 #define BME680_SPI_CS_PIN 17
+/** BME680 SPI Mode Toggle Pin */
 #define BME680_SPI_MODE 0
 
-// icm 20948 imu
+/** ICM20948 IMU CS Pin */
 #define ICM_CS_PIN 26
+/** ICM20948 IMU I2C Toggle Pin */
 #define ICM_I2C_MODE 1
 
-// lsm9ds1 imu
+/** LSM9DS1 IMU Accelerometer/Gyroscope Pin */
 #define LSM9DS1_XGCS_PIN 21
+/** LSM9DS1 IMU Magnetometer Pin */
 #define LSM9DS1_MCS_PIN 22
 
 // storages
-// radio
+// for radio
+/** UART0 RX Pin */
 #define SERIAL1_RX_PIN 1
+/** UART0 RX Pin */
 #define SERIAL1_TX_PIN 0
 
-// sd card
+/** SD Card SPI Toggle */
 #define SD_SPI1 1
+/** SD Card SPI CS PIN */
 #define SD_CS_PIN 9
 
 // spi1
+/** SPI1 MISO PIN */
 #define SPI1_MISO_PIN 8
+/** SPI1 SCK PIN */
 #define SPI1_SCK_PIN 10
+/** SPI1 MOSI PIN */
 #define SPI1_MOSI_PIN 11
 
 // main pin definitions
+/** Built-in LED Pin */
 #define ON_BOARD_LED_PIN 25
+/** Core 0 Heartbeat Pin */
 #define HEARTBEAT_PIN_0 12
+/** Core 1 Heartbeat Pin */
 #define HEARTBEAT_PIN_1 13
+/** Flash Data Interface Enable Pin  */
 #define DATA_INTERFACE_PIN 15
 
 #endif
