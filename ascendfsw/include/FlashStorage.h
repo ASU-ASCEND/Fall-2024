b05e3d091a66083c8c8c845c7a9c5d6e7f9e79bb
BryceVerberne <bryceverberne@gmail.com>
2024-11-14 17:10:26 -0700

    Include & instantiate vital flash libraries

--- ascendfsw/include/FlashStorage.h
+++ ascendfsw/include/FlashStorage.h
@@ -1,16 +1,23 @@
 #ifndef FLASH_STORAGE_H
 #define FLASH_STORAGE_H
 
+#include <SPI.h>
 #include "Storage.h"
+#include "PayloadConfig.h"
+#include "SparkFun_SPI_SerialFlash.h"
 
 class FlashStorage : public Storage {
     private:
         String file_name;
+        SFE_SPI_FLASH flash;
+        String storage_name = "Flash Storage";
+        SPIClassRP2040 sd_spi_1 = SPIClassRP2040(spi1, SPI1_MISO_PIN, SD_CS_PIN,
+                                                 SPI1_SCK_PIN, SPI1_MOSI_PIN);
 
     public:
         FlashStorage();
-        bool verify();
-        void store(String data);
+        bool verify() override;
+        void store(String) override;
 };
 
 #endif 
